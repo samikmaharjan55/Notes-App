e4631589d08793f8eb454804f1de7b73a2d5903b
Samik Maharjan <samikmaharjan55@gmail.com>
2023-08-09 16:24:53 +0545

    Insert Data

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:notes_app/boxes/boxes.dart';
+import 'package:notes_app/models/notes_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +23,65 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {},
+        onPressed: () async {
+          _showMyDialog();
+        },
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('ADD NOTES'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                      hintText: 'Description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    final data = NotesModel(
+                        title: titleController.text,
+                        description: descriptionController.text);
+                    final box = Boxes.getData();
+                    box.add(data);
+                    data.save();
+                    titleController.clear();
+                    descriptionController.clear();
+                    print(box);
+                    Navigator.pop(context);
+                  },
+                  child: Text('Add')),
+            ],
+          );
+        });
   }
 }
