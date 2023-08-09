35bc9698b8546fbf47f688cd66797300503db141
Samik Maharjan <samikmaharjan55@gmail.com>
2023-08-09 16:52:45 +0545

    Completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/boxes/boxes.dart';
 import 'package:notes_app/models/notes_model.dart';
@@ -18,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notes App'),
+        title: const Text('Notes App'),
       ),
       body: ValueListenableBuilder<Box<NotesModel>>(
         valueListenable: Boxes.getData().listenable(),
@@ -29,20 +28,50 @@ class _HomeScreenState extends State<HomeScreen> {
               shrinkWrap: true,
               itemCount: box.length,
               itemBuilder: (context, index) {
-                return Container(
-                  height: 80,
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 15, horizontal: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(data[index].title.toString()),
-                          Text(data[index].description.toString()),
-                        ],
-                      ),
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              data[index].title.toString(),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 20),
+                            ),
+                            const Spacer(),
+                            InkWell(
+                              onTap: () {
+                                delete(data[index]);
+                              },
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            InkWell(
+                                onTap: () {
+                                  _editDialog(
+                                      data[index],
+                                      data[index].title.toString(),
+                                      data[index].description.toString());
+                                },
+                                child: const Icon(Icons.edit)),
+                          ],
+                        ),
+                        Text(
+                          data[index].description.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 18),
+                        ),
+                      ],
                     ),
                   ),
                 );
@@ -53,33 +82,40 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () async {
           _showMyDialog();
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
 
-  Future<void> _showMyDialog() async {
+  void delete(NotesModel notesModel) async {
+    await notesModel.delete();
+  }
+
+  Future<void> _editDialog(
+      NotesModel notesModel, String title, String description) async {
+    titleController.text = title;
+    descriptionController.text = description;
     return showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text('ADD NOTES'),
+            title: const Text('EDIT NOTES'),
             content: SingleChildScrollView(
               child: Column(
                 children: [
                   TextFormField(
                     controller: titleController,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       hintText: 'Enter Title',
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                   TextFormField(
                     controller: descriptionController,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       hintText: 'Description',
                       border: OutlineInputBorder(),
                     ),
@@ -92,7 +128,58 @@ class _HomeScreenState extends State<HomeScreen> {
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text('Cancel')),
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () async {
+                    notesModel.title = titleController.text.toString();
+                    notesModel.description =
+                        descriptionController.text.toString();
+                    notesModel.save();
+                    titleController.clear();
+                    descriptionController.clear();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Edit')),
+            ],
+          );
+        });
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('ADD NOTES'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: const InputDecoration(
+                      hintText: 'Description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
               TextButton(
                   onPressed: () {
                     final data = NotesModel(
@@ -104,7 +191,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     descriptionController.clear();
                     Navigator.pop(context);
                   },
-                  child: Text('Add')),
+                  child: const Text('Add')),
             ],
           );
         });
